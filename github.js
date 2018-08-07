e43b5f9dd3598529c5d19d0836ed426343eaa96c
Dijana <deny.dijana@gmail.com>
2018-08-07 16:51:59 +0200

    Update github.js

--- github.js
+++ github.js
@@ -1,16 +1,22 @@
 class Github {
   constructor() {
-    this.client_id = 'b41f959e3e091c2cc482';
-    this.client_secret = '4e8e1c4e06b5a3ae164e405813016305f3283c21';
+    this.client_id = 'd9308aacf8b204d361fd';
+    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
